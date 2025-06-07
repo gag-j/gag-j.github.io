703e77086fe781bb01175a3e171b29db28a8b066
Gagan Jain <gaganjain1582@gmail.com>
2025-06-08 01:10:22 +0530

    rewamp

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to your restored Next.js site</h1>
+      <p>This is a minimal starter page. Add your content here.</p>
+    </main>
+  );
+}
